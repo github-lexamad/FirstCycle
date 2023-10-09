b066741d7e816bdc3b3b1a584bce1bfb899b527a
LexaMAD <aleksis.telnovs@marupe.edu.lv>
2023-10-09 15:27:28 +0300

    Login logic added
    wery mad bruw

--- proc.php
+++ proc.php
@@ -1,6 +1,13 @@
 <?php
 $l = $_POST["login"];
 $p = $_POST["password"];
-echo "$l, HELLO, $p";
 
+include("cfg.php");
+$result = mysqli_query($mysqli, "SELECT * FROM users WHERE login=$l AND password=$p");
+if(mysqli_num_rows($result) > 0){
+    echo "Inside";
+}
+else{
+    echo "Outside";
+}
 ?>
